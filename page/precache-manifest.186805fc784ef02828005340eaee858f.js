01d6bca0ddc7283827c966be03a55a662e81eb30
Yael <yaelduckwen@gmail.com>
2021-04-20 12:00:31 -0300

    pages

---
+++ page/precache-manifest.186805fc784ef02828005340eaee858f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d16a9d2bdf466026c0b",
+    "url": "/css/app.686688fc.css"
+  },
+  {
+    "revision": "254fd9b7538d2f1f62add6ba7ba4fb4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51af70daef6dbbc09c4d",
+    "url": "/js/about.e7763a82.js"
+  },
+  {
+    "revision": "8d16a9d2bdf466026c0b",
+    "url": "/js/app.9fbc3f5a.js"
+  },
+  {
+    "revision": "180f40350c17e363cb00",
+    "url": "/js/chunk-vendors.2b3c3abe.js"
+  },
+  {
+    "revision": "e5e787051270c35b92bb",
+    "url": "/js/index.1d154e06.js"
+  },
+  {
+    "revision": "48d884930a04c88b073e",
+    "url": "/js/read.51fb1553.js"
+  },
+  {
+    "revision": "2bef094aee76f8d636d358e6a6e4d896",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
